db8a4a5b5aa758c9a8b0b23020831a8f32fdfcaa
qusaijamous12 <qqjamous12@gmail.com>
2024-11-11 13:58:42 +0300

    NEW

--- lib/views/user_screen/about_app_screen.dart
+++ lib/views/user_screen/about_app_screen.dart
@@ -13,22 +13,21 @@ class AboutAppScreen extends StatelessWidget {
       appBar: AppBar(title: Text('About_App '.tr,style:const TextStyle(color: Colors.white),),backgroundColor: kPrimary,leading: IconButton(onPressed: (){
         Get.back();
       }, icon:const Icon(Icons.arrow_back_ios,color: Colors.white,)),),
-      body:SingleChildScrollView(
-
-        child: Column(
-          children: [
-           const Padding(
+      body:Column(
+        children: [
+          Expanded(child: Image.asset('assets/images/about_app.png',height:500,fit: BoxFit.fill,width: double.infinity,)),
+          const Expanded(
+            child:  Padding(
               padding:  EdgeInsets.all(20.0),
               child: Text('About Us Welcome to the University Sports Club app! Here you will find everything you need to stay healthy and fit. Our app aims to promote a healthy and active lifestyle among students and staff. And to facilitate communication and knowledge of the latest events and activities. We offer a wide range of sporting activities, from casual games to competitive teams, all in a fun and inclusive environment. Our Mission To encourage fitness, teamwork and sportsmanship while providing opportunities for students to enjoy a variety of sporting activities, stay healthy and make new friends. Join us to stay active, stay healthy and create lasting memories as part of our vibrant university community!',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.w600
-              ),),
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600
+                ),),
             ),
-            Image.asset('assets/images/about_app.png',fit: BoxFit.fill,)
-          ],
-        ),
+          ),
+        ],
       ) ,
 
     );
